cdee1196663f8bb29b4cea7e0c4018175117bd16
JustinFR <justin@gentlenews.com>
2023-05-08 14:20:23 -0500

    dev mode enabled, logging

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
+const createError = require('http-errors');
 const express = require('express');
-const PORT = process.env.PORT || 3000;
-const taskRoutes = require('./routes/taskRoutes');
+const path = require('path');
+const logger = require('morgan');
+
+const taskRouter = require('./routes/taskRoutes');
 
 const app = express();
 
-const schedule = require('node-schedule');
-const taskService = require('./services/taskService');
+const port = process.env.PORT || 3000;
 
-const dailyResetRule = new schedule.RecurrenceRule();
-dailyResetRule.hour = 2;
-dailyResetRule.minute = 0;
-
-schedule.scheduleJob(dailyResetRule, () => {
-  console.log('Performing daily reset');
-  taskService.resetDailyTasks();
-});
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public')); // Add this line
-
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/', taskRoutes);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-  console.log('Server is running on port 3000');
+app.use((req, res, next) => {
+  res.locals.isDev = req.headers.host.includes('localhost');
+  next();
+});
+
+app.use('/', taskRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
