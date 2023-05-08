d2472b06e91ecf58cefb00a6ffad9b7a3b477705
JustinFR <justin@gentlenews.com>
2023-05-08 18:44:11 -0500

    Errors and uptime

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const startTime = new Date();
 const path = require('path');
 const logger = require('morgan');
 
@@ -20,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   res.locals.isDev = req.headers.host.includes('localhost');
+  res.locals.serverStartTime = startTime;
   next();
 });
 
@@ -40,6 +42,18 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 
 module.exports = app;
 
